5c721c78ecb8b4faed40e8b21f06504c656f209a
magdev022 <magdev022@gmail.com>
2023-09-08 10:26:45 +0300

    написал функцию для расчета четвертной оценки

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function sum($summFirst,$summSecond,$summmThird,$summFourth,$summFifth ){
+    $sum= ($summFirst + $summSecond + $summmThird + $summFourth + $summFifth)/5;
+ return   print_r($sum);
+}
+
+
+?>
